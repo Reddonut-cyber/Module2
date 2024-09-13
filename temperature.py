e3acfe73d64e12e448076e80a91ee41d0851fdaa
Reddonut-cyber <akhowsuk@gmail.com>
2024-09-13 13:20:03 +0700

    worm.2

--- temperature.py
+++ temperature.py
@@ -9,13 +9,18 @@ c = 30
 k = 20
 f = c * 2
 a = f * 5 + 10
-
+seeworm = '#00FF33'
+haoworm = 'yellow'
+kuy = '#FF0033'
+turtle.color('black', seeworm )
+turtle.begin_fill()
+turtle.shape('turtle')
 turtle.pensize(5)
-turtle.speed(5)
+turtle.speed(2)
 
 
 turtle.penup()
-turtle.forward(20)
+turtle.forward(-20)
 turtle.pendown()
 turtle.circle(c)
 turtle.right(45)
@@ -74,14 +79,18 @@ turtle.forward(k)
 turtle.backward(k)
 turtle.left(135)
 turtle.penup()
+turtle.end_fill()
 
 turtle.forward(45)
 turtle.left(90)
 turtle.forward(15)
 turtle.right(90)
 turtle.pendown()
+turtle.color('black', haoworm)
+turtle.begin_fill()
 turtle.circle(15)
 turtle.penup()
+turtle.end_fill()
 
 turtle.right(90)
 turtle.forward(15)
@@ -90,7 +99,12 @@ turtle.backward(a)
 turtle.left(90)
 turtle.forward(60)
 turtle.pendown()
+turtle.color('black', haoworm)
+turtle.begin_fill()
 turtle.circle(c)
+turtle.end_fill()
+
+
 
 turtle.penup()
 turtle.right(90)
@@ -100,12 +114,22 @@ turtle.forward(30)
 turtle.pendown()
 turtle.right(45)
 turtle.forward(45)
+turtle.color('black', kuy)
+turtle.begin_fill()
 turtle.circle(-6)
+turtle.end_fill()
 turtle.backward(45)
 turtle.left(90)
 turtle.forward(45)
+turtle.color('black', kuy)
+turtle.begin_fill()
 turtle.circle(6)
+turtle.end_fill()
 turtle.backward(45)
+turtle.penup()
+turtle.forward(222)
+
+
 
 
 
